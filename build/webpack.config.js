13b7f6c293f6da5081eaf6d9898e1e5d49730e46
liuxin <784532927@qq.com>
2017-12-16 15:17:07 +0800

    webpack loader基础配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HTMLPLUGIN = require('html-webpack-plugin');
 module.exports = {
     entry: {
         app: path.join(__dirname, '../client/app.js')
@@ -9,7 +10,32 @@ module.exports = {
 
         path: path.join(__dirname, '../dist'),
 
-        publicPath: "",
+        // publicPath: "",
         // 区分静态资源, 还是api请求
-    }
+    },
+
+    module: {
+        rules: [
+            // loaders
+            {
+                test: /.jsx$/,
+                loader: 'babel-loader'
+                // babel-loader支持jsx的编译, jsx官方默认编译工具
+                // babel-loader是一个webpack的插件, 还需要babel的核心代码babel-core
+                // babel需要配置, 默认编译es6的代码
+            },
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                exclude: [
+                    path.join(__dirname, '../node_modules')
+                ]
+            }
+
+        ]
+    },
+
+    plugins: [
+        new HTMLPLUGIN()
+    ]
 }
--- client/app.js
+++ client/app.js
@@ -1,1 +1,8 @@
-alert(1);
+import React from 'react';
+// jsx最后编译的时候会使用React.createElement, 所以必须在这里引入
+
+import ReactDOM from 'react-dom';
+
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.body);
